ee1dc1635043f0c4de82f8d342b1cfc8cbfc896d
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-01-16 10:56:18 +0100

    Create     focus-search-box-keyboard-shortcut-youtube.js

---
+++ focus-search-box-keyboard-shortcut-youtube.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        : to focus the search box
+// @namespace   Violentmonkey Scripts
+// @match       https://www.youtube.com/
+// @grant       none
+// @version     1.0
+// @author      thomaslinux
+// @description 16/01/2026 10:33:34
+// ==/UserScript==
+document.addEventListener('keydown', function(event) {
+    if (event.key === ':') {
+        const input = document.querySelector('.ytSearchboxComponentInput');
+        if (input) {
+            input.focus();
+            event.preventDefault(); // Empêche le comportement par défaut de la touche si nécessaire
+        }
+    }
+});
